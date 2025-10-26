f5aa3d81f9dbb861007e2c999bd8d36a5a9df9e4
Alex30340 <alex.navarro30340@gmail.com>
2025-10-26 12:27:24 +0100

    Delete aidgest_project_full_fixed_and_deployable directory

--- aidgest_project_full_fixed_and_deployable/backend/models/return.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ReturnSchema = new mongoose.Schema({
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  amount: { type: Number, required: true },
-  description: { type: String },
-});
-
-module.exports = mongoose.model('Return', ReturnSchema);
